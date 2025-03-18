513787b9af176577a9ec8dee939e365d83786f9b
Saran Vignesh <153472653+Saran-067@users.noreply.github.com>
2025-03-18 15:46:32 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,10 @@ import { Provider } from 'react-redux'
 import store from './redux/store.js'
 import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
+import axios from "axios";
+
+// Enable credentials for all Axios requests
+axios.defaults.withCredentials = true;
 
 const persistor = persistStore(store);
 
