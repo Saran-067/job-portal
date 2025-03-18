f3d38f9f1c3de4450b0e9a864b95bbbb562e8035
Saran Vignesh <153472653+Saran-067@users.noreply.github.com>
2025-03-18 15:34:20 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000/api/v1";
+export const BASE_URL = "https://job-portal-xs9p.onrender.com/api/v1";
 
 export const USER_API_END_POINT = `${BASE_URL}/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/job`;
