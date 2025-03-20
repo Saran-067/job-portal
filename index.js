fcb1698f1c74a5756dc2657f0f42aeb253aebfe4
Saran-067 <saranrajasekaran067@gmail.com>
2025-03-20 07:11:20 +0530

    final

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
-
+import connectDB from "./backend/utils/db.js";
+import userRoute from "./backend/routes/user.route.js";
+import companyRoute from "./backend/routes/company.route.js";
+import jobRoute from "./backend/routes/job.route.js";
+import applicationRoute from "./backend/routes/application.route.js";
+import path from "path";
+import nodemon from "nodemon";
 dotenv.config({});
 
 const app = express();
-
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -32,8 +34,9 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
+app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
